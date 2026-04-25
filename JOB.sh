578dd62d0dece9bba21bc6b3f852cfa419c9c455
Asher Bloom <si23870@bristol.ac.uk>
2026-04-25 15:09:26 +0100

    fix slurm

--- JOB.sh
+++ JOB.sh
@@ -14,8 +14,8 @@ make clean
 make all
 
 echo "1024x1024"
-srun --mpi=cray_shasta ./perf_wrapper.sh d2q9-bgk ../d2q9-bgk-inputs/input_1024x1024.params ../d2q9-bgk-inputs/obstacles_1024x1024.dat
+srun --mpi=cray_shasta ./perf_wrapper.sh ./d2q9-bgk ../d2q9-bgk-inputs/input_1024x1024.params ../d2q9-bgk-inputs/obstacles_1024x1024.dat
 echo "2048x2048"
-srun --mpi=cray_shasta ./perf_wrapper.sh d2q9-bgk ../d2q9-bgk-inputs/input_2048x2048.params ../d2q9-bgk-inputs/obstacles_2048x2048.dat
+srun --mpi=cray_shasta ./perf_wrapper.sh ./d2q9-bgk ../d2q9-bgk-inputs/input_2048x2048.params ../d2q9-bgk-inputs/obstacles_2048x2048.dat
 echo "4096x4096"
-srun --mpi=cray_shasta ./perf_wrapper.sh d2q9-bgk ../d2q9-bgk-inputs/input_4096x4096.params ../d2q9-bgk-inputs/obstacles_4096x4096.dat
+srun --mpi=cray_shasta ./perf_wrapper.sh ./d2q9-bgk ../d2q9-bgk-inputs/input_4096x4096.params ../d2q9-bgk-inputs/obstacles_4096x4096.dat
