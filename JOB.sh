cec5f2459f98771991ac88719e218586ed37687c
Asher Bloom <si23870@bristol.ac.uk>
2026-04-26 18:02:38 +0100

    update slurm

--- JOB.sh
+++ JOB.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=job
 #SBATCH --output=OUTPUT.out
 #SBATCH --nodes=4
-#SBATCH --ntasks-per-node=8
-#SBATCH --cpus-per-task=18
+#SBATCH --ntasks-per-node=16
+#SBATCH --cpus-per-task=9
 #SBATCH --time=00:02:00
 #SBATCH --exclusive
 
@@ -16,8 +16,8 @@ make clean
 make all
 
 echo "1024x1024"
-srun --mpi=cray_shasta ./perf_wrapper.sh ./d2q9-bgk ../d2q9-bgk-inputs/input_1024x1024.params ../d2q9-bgk-inputs/obstacles_1024x1024.dat
+srun --mpi=cray_shasta --cpus-per-task="$SLURM_CPUS_PER_TASK" ./perf_wrapper.sh ./d2q9-bgk ../d2q9-bgk-inputs/input_1024x1024.params ../d2q9-bgk-inputs/obstacles_1024x1024.dat
 echo "2048x2048"
-srun --mpi=cray_shasta ./perf_wrapper.sh ./d2q9-bgk ../d2q9-bgk-inputs/input_2048x2048.params ../d2q9-bgk-inputs/obstacles_2048x2048.dat
+srun --mpi=cray_shasta --cpus-per-task="$SLURM_CPUS_PER_TASK" ./perf_wrapper.sh ./d2q9-bgk ../d2q9-bgk-inputs/input_2048x2048.params ../d2q9-bgk-inputs/obstacles_2048x2048.dat
 echo "4096x4096"
-srun --mpi=cray_shasta ./perf_wrapper.sh ./d2q9-bgk ../d2q9-bgk-inputs/input_4096x4096.params ../d2q9-bgk-inputs/obstacles_4096x4096.dat
+srun --mpi=cray_shasta --cpus-per-task="$SLURM_CPUS_PER_TASK" ./perf_wrapper.sh ./d2q9-bgk ../d2q9-bgk-inputs/input_4096x4096.params ../d2q9-bgk-inputs/obstacles_4096x4096.dat
